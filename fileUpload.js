c1f2ed30b735f5eb240f925914f1f502961287b1
Prateek Nagar <prateeknagar1705@gmail.com>
2018-02-21 15:48:43 -0500

    File parsed using using formidable

--- fileUpload.js
+++ fileUpload.js
@@ -1,10 +1,25 @@
 var http = require('http');
 
+var formidable = require('formidable');
+
+
+
 http.createServer(function (req, res){
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-  res.write('<input type="file" name="filetoupload"><br>');
-  res.write('<input type="submit">');
-  res.write('</form>');
-  return res.end();
+	if(req.url =="/fileupload") {
+		var form = new formidable.IncomingForm();
+		form.parse(req, function (err, fields, files) {
+			//var oldpath = files.filetoupload.path;
+		//console.log(oldpath);
+		res.write('File uploaded');
+		res.write('Temp folder path' + files.filetoupload.path)
+		res.end();
+		});			
+	} else {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+		res.write('<input type="file" name="filetoupload"><br>');
+		res.write('<input type="submit">');
+		res.write('</form>');
+		return res.end();
+	}  
 }).listen(8080);
